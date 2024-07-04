ea9940370947530c92ca91d74fb267fdf4ced5a3
s24014 <s24014@std.it-college.ac.jp>
2024-07-04 07:58:01 +0000

    3日

---
+++ 2-6.py
@@ -0,0 +1,14 @@
+a = "abc"
+b = "xyz"
+"{}{}".format(a, b)
+
+title = 'name'
+value = 'baka'
+print( 'your {} is {}. '.format(title, value))
+
+age = 160
+print( 'your age is {}. '.format(age))
+
+title = 'IQ'
+value = 30
+print('{} = {}'.format(title, value))
---
+++ 2-8.py
@@ -0,0 +1,23 @@
+my_list =[0, 1, 2, 3, 4, 5, 6, 7, 8]
+print(my_list[2])
+
+print(my_list[-1])
+print(my_list[2:7])
+print(my_list[2:7:2])
+print(my_list[:])
+print(my_list[::-1])
+print(my_list[6:100])
+print(my_list[50:100])
+
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+my_list[2:4] = [101, 102, 103, 104]
+print(my_list)
+
+my_list =[0, 1, 2, 3, 4, 5, 6, 7, 8]
+my_list[2:6] = [101, 102]
+print(my_list)
+
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+my_list[2:6] = []
+print(my_list)
+
