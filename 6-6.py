d6e4e8ad633c1a5c501287e3e80b31db9dbb14b3
s24014 <s24014@std.it-college.ac.jp>
2024-08-06 11:58:37 +0000

    7章の始まり

--- 6-6.py
+++ 6-6.py
@@ -1,1 +1,30 @@
+# グローバルスコープ
+def scope_test():
+    # ネステッドスコープ(scope_testのローカルスコープ)
+    def do_local():
+        # ローカルスコープ
+        s1 = "local     "
+    def do_nonlocal():
+        # ローカルからネステッドへスコープを移す
+        nonlocal s2
+        # ネステッドスコープ
+        s2 = "nonlocal"
+    def do_global():
+        # ローカルからグローバルへスコープを移す
+        global s3
+        # グローバルスコープ
+        s3 = "global  "
+    # ネステッドスコープ(scope_testのローカルスコープ)
+    s0 = s1 = s2 = s3 = "test      "
+    do_local()
+    print("After local    :", s0, s1, s2, s3)
+    do_nonlocal()
+    print("After local    :", s0, s1, s2, s3)
+    do_global()
+    print("After global   :", s0, s1, s2, s3)
 
+# グローバルスコープ
+s0 = s1 = s2 = s3 = "initial "
+print("In the global  :", s0, s1, s2, s3)
+scope_test()
+print("After func call:", s0, s1, s2, s3)
